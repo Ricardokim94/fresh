baa4b94304b550dc385c2467e142628bd46ad662
Ricardokim94 <cmrikim@naver.com>
2024-06-19 08:31:52 +0900

    page layout

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
+  let name = 'Ricardo' //변수 사용법
   return (
     <div>
-      
+        <h4 className="title">Test_Page</h4>
+        <p className="title-sub">{name}의 테스트 페이지 입니다.</p>
     </div>
   );
 }
