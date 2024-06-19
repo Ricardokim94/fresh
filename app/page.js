298bad8de54f6693522c47ac4ce7000b23cbc673
Ricardokim94 <cmrikim@naver.com>
2024-06-19 09:18:18 +0900

    Routing / layout.js에 navbar따로 뺴기

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import Link from "next/link"; //a tag말고 Link tag 사용하면 조금더 부드러운 화면 전환 됨.
+
 export default function Home() {
   let name = 'Ricardo' //변수 사용법
   return (
     <div>
-        <h4 className="title">Test_Page</h4>
-        <p className="title-sub">{name}의 테스트 페이지 입니다.</p>
+      
+      <h1 className="title">Test Page</h1>
+      <p className="title-sub">by dev {name}</p>
     </div>
-  );
+  )
 }
