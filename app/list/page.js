2cafc4bffafff91f1e4e1db9d24a97b5bf0bb2bd
Ricardokim94 <cmrikim@naver.com>
2024-06-19 09:49:50 +0900

    map을 사용하여 반복

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,20 @@
 export default function List() {
+    //지금은 DB가 없으므로 data를 일단 임의로 넣어봄
+    let Product = ['Tomatoes', 'Pasta', 'Coconut']
+
     return (
-      <div>
-        <h2 className="title">Products</h2>
-        <div className="food">
-          <h4>상품명 $40</h4>
+        <div>
+            <h2 className="title">Products</h2>
+            {/* map으로 for문 사용(반복) */}
+            {
+                Product.map((item, i) => {
+                    return (
+                        <div className="food" key={i}>
+                            <h4>{item}</h4>
+                        </div>
+                    );
+                })
+            }
         </div>
-        <div className="food">
-          <h4>상품명 $40</h4>
-        </div>
-      </div>
-    )
-  } 
+    );
+} 
