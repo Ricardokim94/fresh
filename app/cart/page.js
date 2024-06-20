92b3d7629ed385f711f31ecb79ad1c1d36b51e9f
Ricardokim94 <cmrikim@naver.com>
2024-06-20 17:20:41 +0900

    component 사용 / 파일분할

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,3 +1,9 @@
+import {age}from './data.js'
+
+            //server component 는 이안에 JS를 넣을 수 없다.
+             //usestate, useEffect 같은거 사용x
+// 'use client' 이렇게 하면 사용할 수 있음. client component
+
 export default function Cart() {
     return (
       <div>
@@ -5,13 +11,26 @@ export default function Cart() {
         <div className="cart-item">
           <p>상품명</p>
           <p>$40</p>
-          <p>1개</p>
+          <p>{age}</p>
         </div>
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+    )
+  } 
+
+
+  //component
+  function CartItem(){
+    return(
         <div className="cart-item">
           <p>상품명</p>
           <p>$40</p>
           <p>1개</p>
         </div>
-      </div>
     )
-  } 
+  }
+
+//NextJS에서는 component가 2개있다.
+// server component / client component
